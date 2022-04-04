85f48331ddecfc5e101152671f2075e77a280e85
Miguel Perez <perezoji@cs.fsu.edu>
2022-04-03 22:30:04 -0400

    happy commit

--- Project3/client.cpp
+++ Project3/client.cpp
@@ -1,6 +1,12 @@
+#include <netdb.h>
+
 #include <stdio.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 int main(int argc, char ** argv) {
 
--- Project3/server.cpp
+++ Project3/server.cpp
@@ -1,4 +1,6 @@
 #include <string>
+#include <fstream>
+#include <filesystem>
 
 #include <netdb.h>
 #include <stdio.h>
@@ -9,9 +11,35 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-
+namespace fs = std::filesystem;
 
 int main(int argc, char ** argv) {
+
+    if (argc != 2) {
+        printf("usage: server.x configration_file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (!fs::exists(argv[1])) {
+        printf("Could not load '%s'\n", argv[1]); 
+        exit(EXIT_FAILURE);
+    }
+    
+    std::string config;
+    std::getline(std::ifstream(argv[1]), config);
+
+    // port: number
+    // finds the index of ':' and grabs the substr till the end of the line 
+    // from there it is converted to an int
+    int portNum = atoi(config.substr(config.find(":") + 1).c_str());
+
+    int socketFD;
+
+    if ((socketFD = socket(AF_INET, SOCK_STREAM, 0) < 0)) {
+        printf("socket error\n");
+        exit(EXIT_FAILURE);
+    }
+
 
 
     return 0;
